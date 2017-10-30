46ac898a664b599f544b9c383e7752d8a1d36be1
Takeshi Akiyama <takeshi.akiyama@iridge.jp>
2017-10-30 10:41:58 +0900

    [OreoNotification] とりあえず動く

--- Java/OreoNotification/app/src/main/java/com/example/takeshi_a/oreonotification/MainActivity.java
+++ Java/OreoNotification/app/src/main/java/com/example/takeshi_a/oreonotification/MainActivity.java
@@ -1,50 +1,103 @@
 package com.example.takeshi_a.oreonotification;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButtonSendNotificationA;
-    private Button mButtonSendNotificationB;
+    private Context mContext;
+    private int mNotificationId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
 
-        // widget設定
-        setupWidget();
+        final Uri uri1 = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.popinfo_alarm);
+        final Uri uri2 = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.test_se);
 
-        // Listener設定
-        setupLisetner();
-    }
+        // Android 8.0 以上の場合は channel を作成
+        final NotificationChannel channel =
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? settingChannel(uri2) : null;
 
-    /**
-     * リスナーを設定します
-     */
-    private void setupLisetner() {
-        mButtonSendNotificationA.setOnClickListener(new View.OnClickListener() {
+        // 通知作成
+        final Notification notification = settingNotitication(mContext, "channel1", uri1);
+
+        // 送信ボタン
+        Button button = (Button) findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-            }
-        });
-        mButtonSendNotificationB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+                // 通知送信
+                sendNotification(channel, notification);
             }
         });
     }
 
-    /**
-     * ウィジェットを設定します
-     */
-    private void setupWidget() {
-        mButtonSendNotificationA = (Button) findViewById(R.id.btn_send_notificationA);
-        mButtonSendNotificationB = (Button) findViewById(R.id.btn_send_notificationB);
+    // 通知送信
+    private void sendNotification(NotificationChannel channel, Notification notification) {
+        NotificationManager manager = (NotificationManager) getSystemService(
+                Context.NOTIFICATION_SERVICE);
+
+        // Android 8.0 以上の場合は channel を設定
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(mNotificationId++, notification);
+    }
+
+    // 通知作成
+    private Notification settingNotitication(Context context, String channelId, Uri uri) {
+
+        return new NotificationCompat.Builder(this, channelId)
+                .setContentTitle("タイトル( 8.0 ~ )")
+                .setContentText("テキスト")
+                .setSmallIcon(R.drawable.small_notification_icon)
+                .setSound(uri, AudioManager.STREAM_NOTIFICATION)
+                .setVibrate(new long[]{0, 1000, 500, 1000, 500})
+                .setChannelId(channelId)
+                .build();
+    }
+
+    // チャンネル作成
+    @TargetApi(Build.VERSION_CODES.O)
+    private NotificationChannel settingChannel(Uri uri) {
+        // オーディオ属性の設定
+        AudioAttributes audioAttributes = new AudioAttributes.Builder().setUsage(
+                AudioAttributes.USAGE_NOTIFICATION).setContentType(
+                AudioAttributes.CONTENT_TYPE_MUSIC).build();
+
+        // チャンネル作成
+        final NotificationChannel channel = new NotificationChannel(
+                "channel1",
+                "チャンネル１",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        channel.setVibrationPattern(new long[]{0, 500, 100});
+        channel.setSound(uri, audioAttributes);
+        channel.setShowBadge(true);
+        channel.setLightColor(Color.GREEN);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        return channel;
     }
 }
